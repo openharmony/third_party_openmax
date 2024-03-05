3c43ff9eea40240829904a10544896f66f1fa328
xuxuehai <xuxuehai2@huawei.com>
2024-03-05 20:20:33 +0800

    commit msg
    Signed-off-by: xuxuehai <xuxuehai2@huawei.com>

---
+++ OMX_Base.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "OMX_Audio.h",
+#include "OMX_Component.h",
+#include "OMX_ComponentExt.h",
+#include "OMX_ContentPipe.h",
+#include "OMX_Core.h",
+#include "OMX_CoreExt.h",
+#include "OMX_Image.h",
+#include "OMX_ImageExt.h",
+#include "OMX_Index.h",
+#include "OMX_IndexExt.h",
+#include "OMX_IVCommon.h",
+#include "OMX_Other.h",
+#include "OMX_Types.h",
+#include "OMX_Video.h",
+#include "OMX_VideoExt.h"
